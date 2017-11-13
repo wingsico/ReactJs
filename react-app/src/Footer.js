b614d80c93cc6c4eb04fb2a47ed59ccecf7327c3
wingsico <wingsico@outlook.com>
2017-11-14 00:10:59 +0800

    react 组件 使用js

---
+++ react-app/src/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <p>React 小书的footer</p>
+      </footer>
+    )
+  }
+}
+
+export default Footer
