09a1f6268aaa06be35ae4a5296c39b92831efc2e
wingsico <wingsico@outlook.com>
2017-11-14 00:10:59 +0800

    react 组件 使用js

---
+++ react-app/src/header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React 小书</h1>
+      </div>
+    )
+  }
+}
+
+export default Header
