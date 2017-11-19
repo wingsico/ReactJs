f396accd4e02fc387a3647388b8810f3858f12e9
wingsico <wingsico@outlook.com>
2017-11-20 00:04:02 +0800

    增强健壮性

--- react-app/src/Comment/CommentApp.js
+++ react-app/src/Comment/CommentApp.js
@@ -10,6 +10,9 @@ class CommentApp extends Component {
     }
   }
   handleSubmitComment(comment) {
+    if (!comment) return
+    if (!comment.username) return alert('请输入用户名')
+    if (!comment.content) return alert('请输入评论内容')
     this.state.comments.push(comment)
     this.setState({
       comments: this.state.comments
